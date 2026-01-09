c456ae0a29ebdd87781edb03bf3fb45179bfc12d
DogNose <dog.nose.rc@gmail.com>
2026-01-09 23:24:44 +0900

    [GREEN] 座標の文字列パースを実装

--- src/position.go
+++ src/position.go
@@ -1,5 +1,7 @@
 package src
 
+import "fmt"
+
 // Position は盤面上の座標を表す型
 type Position struct {
 	Col int // 列 (0-7: a-h)
@@ -9,4 +11,21 @@ type Position struct {
 // IsValid は座標が有効範囲内かどうかを判定する
 func (p Position) IsValid() bool {
 	return p.Col >= 0 && p.Col < 8 && p.Row >= 0 && p.Row < 8
+}
+
+// ParsePosition は文字列（例: "d4"）を座標に変換する
+func ParsePosition(s string) (Position, error) {
+	if len(s) != 2 {
+		return Position{}, fmt.Errorf("invalid position format: %s", s)
+	}
+
+	col := int(s[0] - 'a')
+	row := int(s[1] - '1')
+
+	pos := Position{Col: col, Row: row}
+	if !pos.IsValid() {
+		return Position{}, fmt.Errorf("position out of range: %s", s)
+	}
+
+	return pos, nil
 }
